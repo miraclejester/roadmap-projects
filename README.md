8f865a3a261dd5c102b4b8273657ab604bf816a6
miraclejester <miraclejester413@gmail.com>
2026-07-05 22:51:41 -0400

    String Formatter

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ Practice projects for roadmap.sh. Index to check all project pages at https://mi
 - Greeting Builder: https://roadmap.sh/projects/js-greeting-builder
 - Temperature Converter: https://roadmap.sh/projects/js-temperature-converter
 - Number Checker: https://roadmap.sh/projects/js-number-checker
+- String Formatter: https://roadmap.sh/projects/js-string-formatter
 
 ## Roadmap
 
---
+++ javascript/string_formatter/strings.js
@@ -0,0 +1,21 @@
+﻿function cleanText(text) {
+    return text.trim();
+}
+
+function capitalize(text) {
+    return `${text.charAt(0).toUpperCase()}${text.slice(1).toLowerCase()}`;
+}
+
+function formatDisplayName(firstName, lastName) {
+    return `${capitalize(cleanText(firstName))} ${capitalize(cleanText(lastName))}`;
+}
+
+const nameForm = document.querySelector('#name-form');
+const nameInput = document.querySelector('#name');
+const surnameInput = document.querySelector('#surname');
+const formattedText = document.querySelector('#formatted-name')
+
+nameForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    formattedText.textContent = formatDisplayName(nameInput.value, surnameInput.value);
+});
